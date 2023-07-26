4c3c756ac9bca34ae00e87d60267be80c6db4b75
SpicyCoder <spk2naveen@hotmail.com>
2023-07-27 03:36:25 +0530

    justify content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,34 +7,37 @@
 </script>
 
 <Article title="Hi There 👋">
-	<p>
-		I'm <b>Naveen</b>, <em>Software Architect</em> with over <em class="text-2xl">14 years</em> of
-		experience in building <Badge type="primary">Web</Badge>, <Badge type="primary">Desktop</Badge>, <Badge
-			type="primary">Service</Badge
-		> & <Badge type="primary">Mobile</Badge> applications
-	</p>
-	<p>
-		Expertise in building <em>distributed applications</em>, using Microsoft Technologies including
-		<Badge type="secondary">.NET</Badge>, <Badge type="secondary">DAPR</Badge> & <Badge
-			type="secondary">Tye</Badge
-		>. Experienced in building end to end applications, that are portable, scalable and maintainable
-	</p>
-	<p>
-		I have worked with SQL based (MS SQL Server, MySQL), NoSQL based (MongoDB), Time Series based
-		(InfluxDB, Prometheus) and multi paradigm (Redis, Elastic Stack) databases and ORMs, such as
-		Entity Framework, Dapper, MongoDB C# Driver and Redis.OM
-	</p>
-	<p>
-		I am an Agile practitioner for over 7 years now, following <Badge type="success">Scrum</Badge>
-		&
-		<Badge type="success">Lean</Badge> methodologies
-	</p>
-	<p>
-		Exposure to JavaScript (meta)frameworks & libraries, building component based frontend
-		applications, using <Badge type="danger">Angular</Badge> and <Badge type="danger"
-			>SvelteKit</Badge
-		>
-	</p>
+	<div class="text-justify">
+		<p>
+			I'm <b>Naveen</b>, <em>Software Architect</em> with over <em class="text-2xl">14 years</em> of
+			experience in building <Badge type="primary">Web</Badge>, <Badge type="primary">Desktop</Badge
+			>, <Badge type="primary">Service</Badge> & <Badge type="primary">Mobile</Badge> applications
+		</p>
+		<p>
+			Expertise in building <em>distributed applications</em>, using Microsoft Technologies
+			including
+			<Badge type="secondary">.NET</Badge>, <Badge type="secondary">DAPR</Badge> & <Badge
+				type="secondary">Tye</Badge
+			>. Experienced in building end to end applications, that are portable, scalable and
+			maintainable
+		</p>
+		<p>
+			I have worked with SQL based (MS SQL Server, MySQL), NoSQL based (MongoDB), Time Series based
+			(InfluxDB, Prometheus) and multi paradigm (Redis, Elastic Stack) databases and ORMs, such as
+			Entity Framework, Dapper, MongoDB C# Driver and Redis.OM
+		</p>
+		<p>
+			I am an Agile practitioner for over 7 years now, following <Badge type="success">Scrum</Badge>
+			&
+			<Badge type="success">Lean</Badge> methodologies
+		</p>
+		<p>
+			Exposure to JavaScript (meta)frameworks & libraries, building component based frontend
+			applications, using <Badge type="danger">Angular</Badge> and <Badge type="danger"
+				>SvelteKit</Badge
+			>
+		</p>
+	</div>
 
 	<p slot="footer">
 		<Button on:click={() => window.open('https://github.com/spicycoder')}
