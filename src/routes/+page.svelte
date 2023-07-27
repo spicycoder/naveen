d7f43b47eca68607d72a4a5e37f5e196eb9b918d
SpicyCoder <spk2naveen@hotmail.com>
2023-07-27 17:54:25 +0530

    Polish content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,33 +9,43 @@
 <Article title="Hi There 👋">
 	<div class="text-justify text-xl">
 		<p>
-			I'm <b>Naveen</b>, <em>Software Architect</em> with over <em class="text-2xl">14 years</em> of
-			experience in building <Badge type="primary">Web</Badge>, <Badge type="primary">Desktop</Badge
+			I'm <b>Naveen</b>, a seasoned <em>Software Architect</em> with more than
+			<em class="text-2xl">14 years</em>
+			of experience specializing in developing a wide range of <Badge type="primary">Web</Badge>, <Badge
+				type="primary">Desktop</Badge
 			>, <Badge type="primary">Service</Badge> & <Badge type="primary">Mobile</Badge> applications
 		</p>
 		<p>
-			Expertise in building <em>distributed applications</em>, using Microsoft Technologies
-			including
-			<Badge type="secondary">.NET</Badge>, <Badge type="secondary">DAPR</Badge> & <Badge
+			My expertise lies in crafting <em>distributed applications</em> using Microsoft Technologies,
+			including <Badge type="secondary">.NET</Badge>, <Badge type="secondary">DAPR</Badge> & <Badge
 				type="secondary">Tye</Badge
-			>. Experienced in building end to end applications, that are portable, scalable and
-			maintainable
+			>. I take pride in creating end-to-end solutions that are not only portable and scalable but
+			also highly maintainable
 		</p>
 		<p>
-			I have worked with SQL based (MS SQL Server, MySQL), NoSQL based (MongoDB), Time Series based
-			(InfluxDB, Prometheus) and multi paradigm (Redis, Elastic Stack) databases and ORMs, such as
-			Entity Framework, Dapper, MongoDB C# Driver and Redis.OM
+			Throughout my career, I have worked extensively with various databases, ranging from SQL-based
+			systems like <em>MS SQL Server</em> and <em>MySQL</em> to NoSQL-based databases like
+			<em>MongoDB</em>. Additionally, I have experience with Time Series databases like
+			<em>InfluxDB</em>
+			and <em>Prometheus</em>, as well as multi-paradigm databases like <em>Redis</em> and
+			<em>Elastic Stack</em>. I am well-versed in using ORMs such as
+			<em>Entity Framework</em>, <em>Dapper</em>, <em>MongoDB C# Driver</em>, and <em>Redis.OM</em>
 		</p>
 		<p>
-			I am an Agile practitioner for over 7 years now, following <Badge type="success">Scrum</Badge>
-			&
-			<Badge type="success">Lean</Badge> methodologies
+			With a strong background in Agile practices, I have been following <Badge type="success"
+				>Scrum</Badge
+			> & <Badge type="success">Lean</Badge> methodologies for over 7 years, ensuring efficient project
+			management and delivery
 		</p>
 		<p>
-			Exposure to JavaScript (meta)frameworks & libraries, building component based frontend
-			applications, using <Badge type="danger">Angular</Badge> and <Badge type="danger"
-				>SvelteKit</Badge
-			>
+			Moreover, I have also delved into the world of JavaScript frameworks and libraries, where I
+			have expertise in building component-based frontend applications using <Badge type="danger"
+				>Angular</Badge
+			> & <Badge type="danger">SvelteKit</Badge>
+		</p>
+		<p>
+			Overall, my passion lies in architecting robust and innovative solutions that align with the
+			latest industry trends and best practices
 		</p>
 	</div>
 
